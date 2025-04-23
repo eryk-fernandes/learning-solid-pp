4b2bff5037b5a7cccc1aff53ad2999929e67d247
Eryk Fernandes <erykfernandespro@gmail.com>
2025-04-22 21:17:03 -0300

    feat: commit inicial

---
+++ src/main/java/org/example/exercicio1/Pedido.java
@@ -0,0 +1,18 @@
+package org.example.exercicio1;
+
+//Refatore o código para que a classe Pedido siga o Princípio da Responsabilidade Única.
+
+public class Pedido {
+
+    public void calculaTotal(){
+        // Lógica de cálculo
+    }
+
+    public void imprimirPedido(){
+        // Lógica para imprimir pedido
+    }
+
+    public void salvarPedido(){
+        // Lógica para salvar no banco
+    }
+}
